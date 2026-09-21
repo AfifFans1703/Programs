3fb8eba9e853790205e9d4fe667f74cbcebae887
AfifFans <mhanchoraf0ankinjar@gmail.com>
2026-09-21 10:21:12 +0700

    Add files via upload

---
+++ Muhammad Afifansyah Setiawan - X PPLG 2/LuasLingkaran/LuasLingkaran/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LuasLingkaran
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat konstanta
+
+            const float Phi = 3.14f;
+
+            Console.WriteLine("== PROGRAM LUAS LINGKARAN ==");
+            Console.Write("Input jari-jari: ");
+            int r = int.Parse(Console.ReadLine());
+
+            var luas = Phi * r * r;
+
+            Console.WriteLine($"Luas Lingkaran = {luas}");
+        }
+    }
+}
